789c8ed2305f11422c74b07ab33216f0207e54d7
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-11-12 20:22:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-function returnFirstTwoDrivers(drivers) {
-  const firstTwoDrivers = function(drivers) {
-    return [drivers[0], drivers[1]];
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
   }
-}
